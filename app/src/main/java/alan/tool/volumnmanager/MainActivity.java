ef39327e6ca42c8a28f01891a851ec131dae0676
Alan <Alan@AlandeMacBook-Pro.local>
2016-05-26 23:19:55 +0800

    Initial commit

---
+++ app/src/main/java/alan/tool/volumnmanager/MainActivity.java
@@ -0,0 +1,13 @@
+package alan.tool.volumnmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
