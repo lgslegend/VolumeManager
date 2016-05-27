dcf3f310d94c6e21399f7e2c5e92cf603b706db2
Alan <freebirdphl@gmail.com>
2016-05-27 18:03:52 +0800

    Add seekbar for volumn control

--- app/src/main/java/alan/tool/volumnmanager/MainActivity.java
+++ app/src/main/java/alan/tool/volumnmanager/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
                 audioMgr.setStreamVolume(AudioManager.STREAM_RING, audioMgr.getStreamMaxVolume(AudioManager.STREAM_RING), AudioManager.FLAG_SHOW_UI);
             }
         });
+
+        SeekBar seekbarRingVol = (SeekBar) findViewById(R.id.seekbar_ring_vol);
+        SeekBar seekbarMediaVol = (SeekBar) findViewById(R.id.seekbar_media_vol);
+        SeekBar seekbarCallVol = (SeekBar) findViewById(R.id.seekbar_call_vol);
+        SeekBar seekbarAlarmVol = (SeekBar) findViewById(R.id.seekbar_alarm_vol);
     }
 
     protected void setVolumn() {
